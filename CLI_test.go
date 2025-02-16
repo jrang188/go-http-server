dd23bce545dd9364be6ff0510083b6cf4070a51c
Justin Ang <justinang177@gmail.com>
2025-02-15 22:53:35 -0800

    added cli and cli test and also put most stubs to testin.go

---
+++ CLI_test.go
@@ -0,0 +1,30 @@
+package poker_test
+
+import (
+	"strings"
+	"testing"
+
+	poker "github.com/jrang188/go-http-server"
+)
+
+func TestCLI(t *testing.T) {
+	t.Run("record 'Chris win' from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record 'Tracy win' from user input", func(t *testing.T) {
+		in := strings.NewReader("Tracy wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Tracy")
+	})
+}
